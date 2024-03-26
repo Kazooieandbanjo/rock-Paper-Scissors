132200928bdb1f106882ce5ac7e834fbea335bd1
Kazooie <Steven_0523@icloud.com>
2024-03-26 13:36:27 -0500

    Made a function that plays one round for paper rock scissors

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function getComputerChoice() {
     let computerChoice;
 
     let randomNumber = Math.floor(Math.random() * 3) + 1;// Randomly chooses 1-3
-    console.log(randomNumber);
 
     if (randomNumber === 1) { // if randomNumber  is 1 then it is rock
         computerChoice = rock;
@@ -20,17 +19,29 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase() // make player choice case insensitive
-    computerSelection = computerSelection.toLowerCase() // make player choice case insensitive
+    playerSelection = playerSelection.toLowerCase(); // make player choice case insensitive
+    computerSelection = computerSelection.toLowerCase(); // make player choice case insensitive
+    console.log(computerSelection);
 
     if (playerSelection === computerSelection) {
         console.log("This is a tie")
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You lose! paper beats rock")
+        console.log("You lose! paper beats rock");
     } else if (playerSelection === "rock" && computerSelection === "scissor") {
-        console.log("You win! rock beats scissor")
+        console.log("You win! rock beats scissor");
+    } else if (playerSelection === "scissor" && computerSelection === "paper") {
+        console.log("You win! scissor beats paper");
+    } else if (playerSelection === "scissor" && computerSelection === "rock") {
+        console.log("You lose! rock beats scissor");
+    } else if (playerSelection === "paper" && computerSelection == "rock") {
+        console.log("You win! paper beats rock");
+    } else {
+        console.log("You lose! scissor beats paper")
     }
 }
+
+const playerSelection = prompt("Please choose one of the following choice Paper, Rock, or Scissors ")
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
