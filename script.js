8c65d59a2106a695703c4a40c8f1a133d9b63bdb
Kazooie <Steven_0523@icloud.com>
2024-03-26 12:05:44 -0500

    Made choices case insensitive and finished the rock paper scissor for rock

--- script.js
+++ script.js
@@ -4,20 +4,33 @@ function getComputerChoice() {
     const scissor = "Scissor";
     let computerChoice;
 
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let randomNumber = Math.floor(Math.random() * 3) + 1;// Randomly chooses 1-3
     console.log(randomNumber);
 
-    if (randomNumber === 1) {
+    if (randomNumber === 1) { // if randomNumber  is 1 then it is rock
         computerChoice = rock;
         return computerChoice;
-    } else if (randomNumber === 2) {
+    } else if (randomNumber === 2) { //if randomNumber  is 2 then it is paper
         computerChoice = paper;
         return computerChoice;
     } else {
-        computerChoice = scissor;
+        computerChoice = scissor; //if randomNumber  is 3 then it is scissor
         return computerChoice;
     }
     return computerChoice;
 }
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase() // make player choice case insensitive
+    computerSelection = computerSelection.toLowerCase() // make player choice case insensitive
+
+    if (playerSelection === computerSelection) {
+        console.log("This is a tie")
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("You lose! paper beats rock")
+    } else if (playerSelection === "rock" && computerSelection === "scissor") {
+        console.log("You win! rock beats scissor")
+    }
+}
