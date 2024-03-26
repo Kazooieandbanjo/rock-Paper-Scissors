36539c546f5e9ae3fd46196d633e5b1228a18b3a
Kazooie <Steven_0523@icloud.com>
2024-03-26 11:52:59 -0500

    created a function to randomly choose rock paper scissors

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function getComputerChoice() {
-    let rock = 1;
-    let paper = 2;
-    let scissor = 3;
+    const rock = "Rock";
+    const paper = "Paper";
+    const scissor = "Scissor";
+    let computerChoice;
 
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber);
+
+    if (randomNumber === 1) {
+        computerChoice = rock;
+        return computerChoice;
+    } else if (randomNumber === 2) {
+        computerChoice = paper;
+        return computerChoice;
+    } else {
+        computerChoice = scissor;
+        return computerChoice;
+    }
+    return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    //Make playerSelection input is case-Insensitive
-    playerSelection = prompt("Please input one of the following choices: Scissor, Paper or Rock.");
-}
+console.log(getComputerChoice())
