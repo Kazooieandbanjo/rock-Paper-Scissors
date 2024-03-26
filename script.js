081f931e8df398080611d642be71541d20b326a3
Kazooie <Steven_0523@icloud.com>
2024-03-26 11:39:35 -0500

    Created a external file to javascript and created a function getComputerChoice

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let rock = 1;
+    let paper = 2;
+    let scissor = 3;
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    //Make playerSelection input is case-Insensitive
+    playerSelection = prompt("Please input one of the following choices: Scissor, Paper or Rock.");
+}
